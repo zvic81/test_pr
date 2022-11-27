b17d5f630c143a6a86fc68016b28b57e481eb21a
zvic <zvic@list.ru>
2022-11-27 20:34:20 +0300

    fich5

--- test.py
+++ test.py
@@ -20,18 +20,5 @@ tasks = [
 def get_tasks():
     return jsonify({'tasks': tasks})
 
-@app.route('/todo/api/v1.0/tasks', methods=['POST'])
-def create_task():
-    if not request.json or not 'title' in request.json:
-        abort(400)
-    task = {
-        'id': tasks[-1]['id'] + 1,
-        'title': request.json['title'],
-        'description': request.json.get('description', ""),
-        'done': False
-    }
-    tasks.append(task)
-    return jsonify({'task': task}), 201
-
 if __name__ == '__main__':
     app.run(debug=False)
